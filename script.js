79a648056ab254ed103f2315000c08777a68dd62
Pavel <hashaafonin2001@mail.ru>
2020-08-19 16:11:21 +0300

    Task1 - Циклы по Ежедневному бюджету

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict'
-
-let money = prompt("Ваш бюджет на месяц?", ""),
+let money = +prompt("Ваш бюджет на месяц?", ""),
   time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
@@ -11,12 +10,75 @@ let appData = {
     timeDate: time,
     savings: false
 } ;
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-	a2 = prompt("Во сколько обойдется?", ""),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-	a4 = prompt("Во сколько обойдется?", "");
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
 
-alert(appData.budget / 30);
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+        && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        console.log ("mistake");
+        i--;
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100 ) {
+    console.log("Минимальный урвоень достатка");
+} else if (appData.moneyPerDay < 2000) {
+    console.log("Средний урвоень достатка");     
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+/* Используем цикл while
+let i = 0;
+while (i < 2) {
+     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+         b = prompt ("Во сколько обойдется?", "");
+
+     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+         console.log ("done");
+
+         appData.expenses[a] = b;
+     } else {
+          console.log ("mistake");
+          i--;
+     }
+
+     i++;
+ } 
+ */
+
+
+ /* Используем цикл Do...while 
+let i = 0;
+do {
+     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+         b = prompt ("Во сколько обойдется?", "");
+
+     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+         console.log ("done");
+
+         appData.expenses[a] = b;
+     } else {
+          console.log ("mistake");
+          i--;
+     }
+
+     i++;
+}
+ while (i < 2); 
+*/
+
